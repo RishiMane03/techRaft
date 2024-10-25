c5d73c88b361b3c0306a1f280e7729c4c5e319c4
Rishikesh Mane <139518215+RishiMane03@users.noreply.github.com>
2024-10-25 10:19:46 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // Set the base path for deployment. If deploying to a root path (like on Vercel or Netlify), you can leave this as '/'.
-  base: '/techRaft/',  // Change '/techRaft/' if deploying at the root or on a different subdirectory
-  
   plugins: [react()],
-
-  // Optional: Configure the build output directory if you want to specify a custom directory
-  build: {
-    outDir: 'dist', // Default output directory
-    sourcemap: true, // Helpful for debugging
-  },
-
-  // Optional: Define server settings if you want to test locally with specific configurations
-  server: {
-    port: 3000, // Set your local dev server port
-    open: true, // Opens the app in the browser on start
-  },
-});
+})
