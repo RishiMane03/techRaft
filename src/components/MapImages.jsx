3845ad200b008925a11c1b6d4d6844ca12cb005f
Rishikesh Mane <139518215+RishiMane03@users.noreply.github.com>
2024-10-25 11:06:38 +0530

    Update MapImages.jsx

--- src/components/MapImages.jsx
+++ src/components/MapImages.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { motion } from 'framer-motion';
 import { animationStart } from '../utils/animation';
+import Map1 from "../assets/map_1.png"
+import Map2 from "../assets/map_2.png"
 
 function MapImages() {
   return (
     <div className="absolute w-full flex justify-between transform -translate-y-20 -z-10 overflow-hidden">
       <motion.img
-        src="src/assets/map_1.png"
+        src={Map1}
         initial={{ x: -100, opacity: 0 }}
         animate={{ x: 0, opacity: 1 }}
         transition={{
@@ -17,7 +19,7 @@ function MapImages() {
         className="w-full h-[250px] max-w-[200px] lg:max-w-[300px] object-cover"
       />
       <motion.img
-        src="src/assets/map_2.png"
+        src={Map2}
         initial={{ x: 100, opacity: 0 }}
         animate={{ x: 0, opacity: 1 }}
         transition={{
